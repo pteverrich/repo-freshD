dd6905ecc194787d4a6bdfa1905a34cdc5fadbf7
pteverrich <pt.everrich@gmail.com>
2026-07-01 14:23:15 +0700

    Update scrip.js
    แก้ตัวบางตัวหน้า ตำแหน่งประโยค

--- script.js
+++ script.js
@@ -191,8 +191,8 @@ function initGame() {
 
   document.getElementById("desc-text").innerHTML = `
     <div class="landing-desc-wrapper">
-      <div class="landing-paragraph">
-        🤢 <strong>ตื่นมาแล้วลืมฉีดสเปรย์... คุณน้าจะเป็นสายไหนในวันนั้น?</strong>
+      <div class="landing-paragraph" style="text-align: center; margin-bottom: 0;">
+        🤢 ตื่นมาแล้วลืมฉีดสเปรย์... คุณน้าจะทำยังไงในวันนั้น?
       </div>
       <div class="landing-paragraph" style="text-align: center; margin-bottom: 0;">
         ✨ <strong>มาเล่นเลย แล้วมาดูกันว่าลึกๆ แล้ว คุณน้าคือมนุษย์สายไหนกันแน่!?</strong>
